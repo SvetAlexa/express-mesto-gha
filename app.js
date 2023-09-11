b0dc0b271ed93f24bc1bd5e1c5011c9815008aa2
SvetAlexa <dazzle@bk.ru>
2023-09-11 20:24:13 +0300

    исправить стайл кода

--- app.js
+++ app.js
@@ -4,17 +4,11 @@ const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true
+  useNewUrlParser: true,
 })
-  .then(() => {
-    console.log('connected to DB')
-  })
-  .catch(() => {
-    console.log('no connection')
-  })
+  .then(() => console.log('connected to DB'))
+  .catch(() => console.log('no connection'));
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
