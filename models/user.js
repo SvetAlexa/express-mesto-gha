55f43d40a6bb0b1a704c34d0792d96c033540ce2
SvetAlexa <dazzle@bk.ru>
2023-09-24 15:49:44 +0300

    добавить email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +17,20 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Введите адрес e-mail в правильном формате',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
   },
 }, { versionKey: false });
 
